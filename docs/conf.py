58e891f7cf6414aef1bd579c38eabfadde1675a4
eduarrom <33223901+eduarrom@users.noreply.github.com>
2018-01-13 19:18:05 +0100

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -165,5 +165,8 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
+#readthedocs theme
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
